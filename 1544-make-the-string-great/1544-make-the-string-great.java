4bd2fc2a0a45b224fb4383c1591ef0d11f8dfa83
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-09-25 09:57:45 +0530

    Time: 1 ms (99.75%), Space: 42.3 MB (63.15%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -2,12 +2,13 @@ class Solution {
     public String makeGood(String s) {
         StringBuilder sb = new StringBuilder();
 
-        for(char ch : s.toCharArray()){
+        for(char c : s.toCharArray()){
             int len = sb.length();
-            if(len > 0 && Math.abs(sb.charAt(len-1) - ch) == 32){
+
+            if(len > 0 && Math.abs(sb.charAt(len-1) - c) == 32){
                 sb.deleteCharAt(len-1);
             }else{
-                sb.append(ch);
+                sb.append(c);
             }
         }
 
