810e3e494f416300a7bb2ba5d0b3a933294c9a02
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-09-29 09:13:16 +0530

    Time: 1 ms (81.87%), Space: 41.5 MB (83.57%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,21 +1,25 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String ver1Arr[] = version1.split("\\.");
-        String ver2Arr[] = version2.split("\\.");
+        String var1Chars[] = version1.split("\\.");
+        String var2Chars[] = version2.split("\\.");
 
-        int n1 = ver1Arr.length;
-        int n2 = ver2Arr.length;
-        int p1=0,p2=0;
+        int p1 = 0;
+        int n1 = var1Chars.length;
+        int p2 = 0;
+        int n2 = var2Chars.length;
+
         while(p1 < n1 || p2 < n2){
-            int num1 = (p1 < n1)?Integer.parseInt(ver1Arr[p1]) : 0;
-            int num2 = (p2 < n2)?Integer.parseInt(ver2Arr[p2]) : 0;
+            int num1 = (p1 < n1)?Integer.parseInt(var1Chars[p1]):0;
+            int num2 = (p2 < n2)?Integer.parseInt(var2Chars[p2]):0;
 
             if(num1 != num2){
-                return (num1 > num2)?1 :-1;
+                return (num1 > num2)?1:-1;
             }
+
             p1++;
             p2++;
         }
+
         return 0;
     }
 }
