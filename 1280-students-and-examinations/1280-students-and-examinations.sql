288f57f1cca0c77d74196db9b412f83881ad6d91
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-11-01 12:45:12 +0530

    Time: 1092 ms (21.19%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,10 +1,29 @@
 # Write your MySQL query statement below
 
-SELECT stu.student_id, stu.student_name, sub.subject_name, COUNT(exam.subject_name) AS attended_exams
-FROM Students stu
+
+-- +------------+--------------+--------------+----------------+
+-- | student_id | student_name | subject_name | attended_exams |
+-- +------------+--------------+--------------+----------------+
+-- | 1          | Alice        | Math         | 3              |
+-- | 1          | Alice        | Physics      | 2              |
+-- | 1          | Alice        | Programming  | 1              |
+-- | 2          | Bob          | Math         | 1              |
+-- | 2          | Bob          | Physics      | 0              |
+-- | 2          | Bob          | Programming  | 1              |
+-- | 6          | Alex         | Math         | 0              |
+-- | 6          | Alex         | Physics      | 0              |
+-- | 6          | Alex         | Programming  | 0              |
+-- | 13         | John         | Math         | 1              |
+-- | 13         | John         | Physics      | 1              |
+-- | 13         | John         | Programming  | 1              |
+-- +------------+--------------+--------------+----------------+
+
+SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.subject_name) AS attended_exams
+-- SELECT *
+FROM Students s
 CROSS JOIN Subjects sub
-LEFT JOIN Examinations exam
-    ON stu.student_id = exam.student_id
-    AND sub.subject_name = exam.subject_name
-GROUP BY stu.student_id, stu.student_name, sub.subject_name
-ORDER BY stu.student_id, sub.subject_name;
+LEFT JOIN Examinations e
+ON s.student_id = e.student_id
+AND sub.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, sub.subject_name
+ORDER BY s.student_id, sub.subject_name
