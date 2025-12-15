f16c227b066a4ea618d48a3d272550f8d4987fec
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-12-15 10:30:26 +0530

    Time: 4 ms (26.72%), Space: 46.2 MB (57.67%) - LeetHub

--- 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.java
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.java
@@ -1,6 +1,7 @@
 class Solution {
     public int mostFrequent(int[] arr, int key) {
         HashMap<Integer, Integer> map = new HashMap<>();
+
         for(int i=0; i<arr.length-1; i++){
             if(arr[i] == key){
                 map.put(arr[i+1], map.getOrDefault(arr[i+1], 0)+1);
@@ -9,10 +10,11 @@ class Solution {
 
         int max = Integer.MIN_VALUE;
         int ans = 0;
-        for(int i : map.keySet()){
-            if(map.get(i) > max){
-                max = map.get(i);
-                ans = i;
+
+        for(int curKey : map.keySet()){
+            if(map.get(curKey) > max){
+                max = map.get(curKey);
+                ans = curKey;
             }
         }
 
