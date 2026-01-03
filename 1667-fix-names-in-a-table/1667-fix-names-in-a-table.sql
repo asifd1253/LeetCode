0451742649656d35d7f2c23423584b4db3d04381
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-01-03 14:31:06 +0530

    Fix name formatting in Users table
    Updated name formatting to capitalize the first letter and lowercase the rest.

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -3,7 +3,7 @@
 SELECT user_id,
        CONCAT(
         UPPER(LEFT(name, 1)),
-        LOWER(RIGHT(name, LENGTH(name)-1))
+        LOWER(RIGHT(name, LENGTH(name)-1))  //Extracts characters from the right side, Takes all characters except the first one
         ) AS name
 FROM Users
 ORDER BY user_id
