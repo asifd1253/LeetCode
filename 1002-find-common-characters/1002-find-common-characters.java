e78ebf00532507ea09820aaf87bf558e22b81743
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-01-13 10:33:47 +0530

    Time: 4 ms (51.38%), Space: 45 MB (35.58%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -1,8 +1,8 @@
 class Solution {
     public List<String> commonChars(String[] words) {
         int csf[] = new int[26];
-        String firstWord = words[0];
 
+        String firstWord = words[0];
         for(char ch : firstWord.toCharArray()){
             int idx = ch - 'a';
             csf[idx]++;
@@ -10,24 +10,28 @@ class Solution {
 
         for(int i=1; i<words.length; i++){
             String curWord = words[i];
-            int curFreq[] = new int[26];
+            int curArr[] = new int[26];
+
             for(char ch : curWord.toCharArray()){
                 int idx = ch - 'a';
-                curFreq[idx]++;
+                curArr[idx]++;
             }
 
-            //update the common so far with min of chars in 2 strings
             for(int j=0; j<26; j++){
-                csf[j] = Math.min(csf[j], curFreq[j]);
+                csf[j] = Math.min(csf[j], curArr[j]);
             }
         }
 
         List<String> res = new ArrayList<>();
         for(int i=0; i<26; i++){
-            int count = csf[i];
-            char ch = (char)(i + 'a');
-            while(count-- > 0){
-                res.add("" + ch);
+        
+            if(csf[i] != 0){
+                char c = (char)(i + 'a');
+                int count = csf[i];
+
+                while(count-- > 0){
+                    res.add("" + c);
+                }
             }
         }
 
